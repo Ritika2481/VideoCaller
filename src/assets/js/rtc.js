b8c4ceb32cbeb9aa187e44d344d3569e53f55747
G C Talukdar <gctalukdar@Gs-MacBook-Pro.local>
2021-07-04 13:56:48 +0530

    removed share screen functns

--- src/assets/js/rtc.js
+++ src/assets/js/rtc.js
@@ -243,47 +243,7 @@ window.addEventListener( 'load', () => {
 
 
 
-        function shareScreen() {
-            h.shareScreen().then( ( stream ) => {
-                h.toggleShareIcons( true );
-
-                //disable the video toggle btns while sharing screen. This is to ensure clicking on the btn does not interfere with the screen sharing
-                //It will be enabled was user stopped sharing screen
-                h.toggleVideoBtnDisabled( true );
-
-                //save my screen stream
-                screen = stream;
-
-                //share the new stream with all partners
-                broadcastNewTracks( stream, 'video', false );
-
-                //When the stop sharing button shown by the browser is clicked
-                screen.getVideoTracks()[0].addEventListener( 'ended', () => {
-                    stopSharingScreen();
-                } );
-            } ).catch( ( e ) => {
-                console.error( e );
-            } );
-        }
-
-
-
-        function stopSharingScreen() {
-            //enable video toggle btn
-            h.toggleVideoBtnDisabled( false );
-
-            return new Promise( ( res, rej ) => {
-                screen.getTracks().length ? screen.getTracks().forEach( track => track.stop() ) : '';
-
-                res();
-            } ).then( () => {
-                h.toggleShareIcons( false );
-                broadcastNewTracks( myStream, 'video' );
-            } ).catch( ( e ) => {
-                console.error( e );
-            } );
-        }
-
+        
 
 
         function broadcastNewTracks( stream, type, mirrorMode = true ) {
@@ -299,11 +259,6 @@ window.addEventListener( 'load', () => {
                 }
             }
         }
-
-
-        
-
-        
 
 
         //Chat textarea
