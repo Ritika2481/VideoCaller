744f415f72f3f89aecb5d7b2059db43cdce2e50b
Ritika Talukdar <ritika.talukdar24@gmail.com>
2021-07-12 00:05:47 +0530

    Debug

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ let express = require( 'express' );
 let app = express();
 let server = require( 'http' ).Server( app );
 let io = require( 'socket.io' )( server );
+console.log(io);
 let stream = require( './ws/stream' );
 let path = require( 'path' );
 let favicon = require( 'serve-favicon' );
